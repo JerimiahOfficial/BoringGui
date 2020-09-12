aff2e9123893e65ec35e8bcd296ade42fd60ab95
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2020-09-12 18:43:44 -0400

    Add files via upload

---
+++ circlebutton.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace MilkyUi
+{
+    public class circlebutton : Button
+    {
+        public circlebutton()
+        {
+            FlatAppearance.BorderSize = 0;
+            FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            TabStop = false;
+        }
+
+        private int _radius = 20;
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.Edgesmoothing();
+
+            GraphicsPath gp = new GraphicsPath();
+            gp.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(gp);
+        }
+    }
+}
---
+++ helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace MilkyUi
+{
+    public static class helper
+    {
+        public static void Edgesmoothing(this Graphics g)
+        {
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+        }
+
+    }
+}
